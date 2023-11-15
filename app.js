9877453001f17899d9e7c1cf288cc1d9a6f81aec
chrisbooth <chris.booth.ct@gmail.com>
2023-11-15 13:21:40 +0200

    Learning to use MongoDB and Mongoose

--- app.js
+++ app.js
@@ -1,24 +1,74 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
-
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
 // Connect to MongoDB
-const dbURI = 'MONGO_PASS'
+const dbURI = process.env.MONGO_PASS
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "node-tuts"
+}
+mongoose.connect(dbURI, connectionParams)
+    .then(() => {
+        console.log("Connected to the database")
+        app.listen(3000);
+    })
+    .catch( (err) => {
+        console.log(`Error connecting to the database. ${err}`)
+    })
 
 // Register view engine
 app.set('view engine', 'ejs')
-
-// listen for requests
-app.listen(3000);
 
 // Middleware and static files
 app.use(express.static('public'))
 app.use(morgan('dev'))
 
+// Mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: "New Blog 2",
+        snippet: "About my new blog",
+        body: "More about my new blog"
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('6554a6f21d2fc3df27f6a447')
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+// routes
 app.get('/', (req, res) => {
     const blogs = [
         {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur"},
